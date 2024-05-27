3a9c2a9e8f9377e9c045c0c56f441e310b02f1da
ros_gen <ros_gen@DESKTOP-0SD6GO5>
2024-05-27 10:56:04 +0300

    Add project files.

---
+++ ArenaGame/Weapons/Axe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGame.Weapons
+{
+    public class Axe : IWeapon
+    {
+        private int Strikes = 0;
+        public string Name { get; set; }
+
+        public double AttackDamage { get; private set; }
+
+        public double BlockingPower { get; private set; }
+
+        public Axe(string name)
+        {
+            Name = name;
+            AttackDamage = 17.5;
+            BlockingPower = 10;
+
+        }
+
+        public void DoubleAttack(Hero Owner)
+        {
+            Strikes++;
+            if (Strikes == 3)
+            {
+                Strikes = 0;
+                AttackDamage += 0.2 * AttackDamage;
+            }
+
+        }
+    }
+}
