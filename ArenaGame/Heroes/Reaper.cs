4e4cadc7bb963cda22a8fe134dc6c84e16bbb364
ros_gen <stu2301321009@uni-plovdiv.bg>
2024-05-28 14:12:50 +0300

    Added a new hero with a chance to avoid 35% damage

---
+++ ArenaGame/Heroes/Reaper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGame.Heroes
+{
+    public class Reaper : Hero
+    {
+        public Reaper(string name, double armor, double strenght, IWeapon weapon) : base(name, armor, strenght, weapon)
+        {
+        }
+
+        
+
+        public override double Defend(double damage)
+        {
+          double probability = random.NextDouble();
+            if(probability < 0.4)
+            {
+                return (base.Defend(damage-(0.35*damage))) ;
+            }
+
+            return base.Defend(damage);
+        }
+    }
+}
